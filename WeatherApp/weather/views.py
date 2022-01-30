43a4495a29fc922bffedf948dfff9999386d636f
Mikalai19 <bertman29@gmail.com>
2022-01-29 22:26:18 -0800

    connect API and rendered info in terminal

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,6 +1,13 @@
+import requests
 from django.shortcuts import render
 
+
 # Create your views here.
+url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=d07e68bcb014a06a4d7b091c1456e711"
+
+city = "Seattle"
+res = requests.get(url.format(city))
+print(res.text)
 
 
 def index(request):
