4f4c6468483b615b062b2ad11d377dff9ec25a93
Mikalai19 <bertman29@gmail.com>
2022-01-30 11:14:54 -0800

    retrieved data from API and created dictonary with necessary data

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -3,11 +3,19 @@ from django.shortcuts import render
 
 
 # Create your views here.
-url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=d07e68bcb014a06a4d7b091c1456e711"
+url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=Fahrenheit&appid=d07e68bcb014a06a4d7b091c1456e711"
 
 city = "Seattle"
-res = requests.get(url.format(city))
-print(res.text)
+res = requests.get(url.format(city)).json()
+
+city_info = {
+    'city': city,
+    'temp': res['main']['temp'],
+    'icon': res['weather']['icon'],
+    'humidity': res['main']['humidity'],
+    'wind': res['wind']['speed'],
+
+}
 
 
 def index(request):
