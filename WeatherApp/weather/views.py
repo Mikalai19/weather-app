74f076d9edef583a122795bcc721ad0d29a41423
Mikalai19 <bertman29@gmail.com>
2022-01-30 12:14:14 -0800

    converd to F and fixed html view

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 # Create your views here.
-url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=Fahrenheit&appid=d07e68bcb014a06a4d7b091c1456e711"
+url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=d07e68bcb014a06a4d7b091c1456e711"
 
 city = "Seattle"
 res = requests.get(url.format(city)).json()
@@ -11,12 +11,17 @@ res = requests.get(url.format(city)).json()
 city_info = {
     'city': city,
     'temp': res['main']['temp'],
-    'icon': res['weather']['icon'],
+    'icon': res['weather'][0]['icon'],
     'humidity': res['main']['humidity'],
     'wind': res['wind']['speed'],
 
 }
+# humidity_info = res['main']['humidity']
+# wind_info = res['wind']['speed']
+# print(humidity_info)
+# print(wind_info)
+context = {'info': city_info}
 
 
 def index(request):
-    return render(request, 'weather/index.html')
+    return render(request, 'weather/index.html', context)
