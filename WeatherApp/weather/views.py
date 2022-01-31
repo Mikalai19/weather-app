85b00549e0491f72e8750e29a36d91e953c7b174
Mikalai19 <bertman29@gmail.com>
2022-01-30 18:28:39 -0800

    fixed the render problem

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -4,36 +4,44 @@ from .models import City
 from .forms import CityForm
 
 
-# Create your views here.
-url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=d07e68bcb014a06a4d7b091c1456e711"
+def index(request):
+    # Create your views here.
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=d07e68bcb014a06a4d7b091c1456e711"
 
+    if(request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
 
-cities = City.objects.all()  # take all data from table
-all_cities = []
+    form = CityForm()
 
-for city in cities:  # iterate all data from the table
+    cities = City.objects.all()  # take all data from table
+    all_cities = []
 
-    res = requests.get(url.format(city.name)).json()  # get data for each city
+    for city in cities:  # iterate all data from the table
+
+        # get data for each city
+        res = requests.get(url.format(city.name)).json()
     # print('City ----->', res)
-    city_info = {
-        'city': city.name,
-        'temp': res['main']['temp'],
-        'icon': res['weather'][0]['icon'],
-        'humidity': res['main']['humidity'],
-        'wind': res['wind']['speed'],
+        city_info = {
+            'city': city.name,
+            'temp': res['main']['temp'],
+            'icon': res['weather'][0]['icon'],
+            'humidity': res['main']['humidity'],
+            'wind': res['wind']['speed'],
 
-    }
+        }
 
     # print('City ----->', city_info['city'])
 
     all_cities.append(city_info)  # insert those data
 
+
 # humidity_info = res['main']['humidity']
 # wind_info = res['wind']['speed']
 # print(humidity_info)
 # print(wind_info)
-context = {'all_info': city_info}  # handed list of cities as param all_info
+# handed list of cities as param all_info
 
+    context = {'all_info': city_info, 'form': form}
 
-def index(request):
     return render(request, 'weather/index.html', context)
