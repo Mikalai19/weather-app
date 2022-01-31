82f52b75fbe9fd271ad799fa3caeb0cfc01c7d18
Mikalai19 <bertman29@gmail.com>
2022-01-30 22:11:18 -0800

    second page

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -22,14 +22,14 @@ def index(request):
         # get data for each city
         res = requests.get(url.format(city.name)).json()
     # print('City ----->', res)
-        city_info = {
-            'city': city.name,
-            'temp': res['main']['temp'],
-            'icon': res['weather'][0]['icon'],
-            'humidity': res['main']['humidity'],
-            'wind': res['wind']['speed'],
+    city_info = {
+        'city': city.name,
+        'temp': res['main']['temp'],
+        'icon': res['weather'][0]['icon'],
+        'humidity': res['main']['humidity'],
+        'wind': res['wind']['speed'],
 
-        }
+    }
 
     # print('City ----->', city_info['city'])
 
@@ -45,3 +45,33 @@ def index(request):
     context = {'all_info': city_info, 'form': form}
 
     return render(request, 'weather/index.html', context)
+
+
+def second(request):
+    url = "http://pro.openweathermap.org/data/2.5/forecast/hourly?q={}&units=imperial&appid=d07e68bcb014a06a4d7b091c1456e711"
+
+    if(request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()  # take all data from table
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+
+        city_info = {
+            'city': city.name,
+            'time': res['sys']['dt_txt'],
+            # 'icon': res['weather'][0]['icon'],
+            # 'humidity': res['main']['humidity'],
+            # 'wind': res['wind']['speed'],
+
+        }
+    all_cities.append(city_info)
+
+    context = {'all_info': city_info, 'form': form}
+
+    return render(request, 'second.html', context)
